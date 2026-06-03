f15d6b120ac3bd90ac59d26029e8d5f477f0b424
juanfdx <juanfdx@gmail.com>
2026-06-03 21:12:42 +0200

    english master v1.0.1.b

--- src/app/(path)/layout.tsx
+++ src/app/(path)/layout.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { Link, NavLink, Outlet } from 'react-router';
 import { ScrollToTop } from '../../components/shared/ScrollToTop';
-
+import {  FaArrowLeftLong } from "react-icons/fa6";
+import { IoMdMenu } from "react-icons/io";
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 const lessonLinks = [
   {
@@ -27,6 +30,18 @@ const lessonLinks = [
 
 
 export default function PathLayout() {
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  const {width} = useWindowSize();
+
+  if (width <= 1024 && isOpen) {
+    setIsOpen(false);
+  }
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
+
   return (
     <div className="min-h-screen flex flex-col bg-linear-to-b from-slate-50 to-white">
       
@@ -56,15 +71,26 @@ export default function PathLayout() {
       <div className="flex-1 flex">
         
         {/* SIDEBAR */}
-        <aside className="hidden md:flex w-72 border-r border-slate-200 bg-white/70 backdrop-blur">
+        <aside className={`hidden md:flex border-r border-slate-200 bg-white/70 backdrop-blur ${isOpen ? "w-72" : "w-27"}`}>
           <div className="w-full p-6">
             
             <div className="mb-8">
-              <h2 className="text-sm font-semibold uppercase tracking-wider text-slate-400 mb-4">
-                Learning Paths
-              </h2>
+              <div className={`text-sm font-semibold uppercase tracking-wider text-slate-400 `}>
+                {isOpen ? (
+                  <div className="flex justify-between items-center mb-4">
+                    <h2>Learning Paths</h2>
+                    <button className='cursor-pointer' onClick={toggleSidebar}>
+                      <FaArrowLeftLong className='text-black w-4 h-4' />
+                    </button>
+                  </div>
+                ) : (
+                  <button className='flex items-center px-3 mb-1 lg:cursor-pointer' onClick={toggleSidebar}>
+                    <IoMdMenu className='text-slate-400 lg:text-black w-8 h-8' />
+                  </button>
+                )}  
+              </div>
 
-              <div className="space-y-2">
+              <nav className="space-y-2">
                 {lessonLinks.map((link) => (
                   <NavLink
                     key={link.path}
@@ -82,12 +108,12 @@ export default function PathLayout() {
                   >
                     <span className="text-xl">{link.icon}</span>
 
-                    <span className="font-medium">
+                    <span className={`font-medium ${isOpen ? "block" : "hidden"}`}>
                       {link.title}
                     </span>
                   </NavLink>
                 ))}
-              </div>
+              </nav>
             </div>
             
           </div>
@@ -95,6 +121,33 @@ export default function PathLayout() {
 
         {/* CONTENT */}
         <main className="flex-1 px-6 pb-12">
+          <nav className='md:hidden flex flex-col items-center gap-3 py-3'>
+            {lessonLinks.map((link) => (
+              <NavLink
+                key={link.path}
+                to={link.path}
+                className={({ isActive }) =>
+                  `
+                  w-full flex items-center gap-3 px-4 py-3 rounded-2xl transition-all duration-200
+                  ${
+                    isActive
+                      ? "bg-indigo-600 text-white shadow-lg shadow-indigo-500/20"
+                      : "text-slate-600 hover:bg-slate-100 hover:text-indigo-600"
+                  }
+                `
+                }
+              >
+                <span className="text-xl">{link.icon}</span>
+
+                <span className={`font-medium`}>
+                  {link.title}
+                </span>
+              </NavLink>
+            ))}
+          </nav>
+
+          <div className="md:hidden border-b border-slate-300 mt-6 mb-3"></div>
+
           <div className="max-w-6xl mx-auto">
             <Outlet />
           </div>
